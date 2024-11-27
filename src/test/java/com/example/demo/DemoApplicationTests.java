aa0b36c4d0d79218a943943628e0447e1547bb59
khuoy borin satya <khuoyborin@gmail.com>
2024-11-27 17:27:18 +0700

    tp5

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//git hub link : https://github.com/rinrin71kh/Sofware_engineering.git
 @SpringBootTest
 class DemoApplicationTests {
 
